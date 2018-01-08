05f7f7d95a6ad3c4548aeb08803d58aa63ec7e2e
Mark Lopez <molopez492@gmail.com>
2018-01-08 17:33:16 -0500

    Redux

--- client/index.js
+++ client/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import {Provider} from 'react-redux'
+import store from './store'
+
+import Root from './components/Root'
+
 render(
-  <div>Test</div>,
+  <Provider store={store}>
+    <Root/>
+  </Provider>,
   document.getElementById('app')
 );
